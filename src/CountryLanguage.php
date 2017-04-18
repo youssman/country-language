535131875cef58d43170aa604e451c67ea10ea20
youssman <haiti.youssef@gmail.com>
2017-04-18 03:21:47 +0200

    fix problem with erased codes

--- src/CountryLanguage.php
+++ src/CountryLanguage.php
@@ -288,9 +288,11 @@ class CountryLanguage {
         }
 
         foreach ($country['languages'] as $language) {
-            $codes[]['iso639_1'] = $language['iso639_1'];
-            $codes[]['iso639_2'] = $language['iso639_2en'];
-            $codes[]['iso639_3'] = $language['iso639_3'];
+            $codes[] = [
+                    'iso639_1' => $language['iso639_1'], 
+                    'iso639_2' => $language['iso639_2en'], 
+                    'iso639_3' => $language['iso639_3']
+                ];
         }
 
         return $codes;
@@ -318,9 +320,11 @@ class CountryLanguage {
         }
 
         foreach ($language['countries'] as $country) {
-            $codes[]['code_2'] = $language['code_2'];
-            $codes[]['code_3'] = $language['code_3'];
-            $codes[]['numCode'] = $language['numCode'];
+            $codes[] = [
+                    'code_2' => $language['code_2'], 
+                    'code_3' => $language['code_3'], 
+                    'numCode' => $language['numCode']
+                ];
         }
 
         return $codes;
